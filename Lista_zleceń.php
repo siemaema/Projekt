25b1eb34393870332c62baebe8bb66bc2a546425
siemak <fkratiuk420@gmail.com>
2024-04-29 18:17:42 +0200

    lista zlecen

--- Lista_zleceń.php
+++ Lista_zleceń.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full">
+<html lang="en" class="h-full ">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +7,50 @@
     <link rel="stylesheet" href="output.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/90bfbfad1c.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
-<body class="h-full">
+<body class="h-full font-Montserrat ">
     <div class="flex h-full w-full bg-[#6b9080]">
         <div>
             <a href="Main.php"><img src="img/back_button.png" alt="xddd" class="object-scale-down h-10 mt-2 ml-2"></a>
         </div>
-        <div class="grid grid-cols-1 grid-rows-7 w-full h-full">
-            <div class="grid place-content-center row-span-1">
-                <p class="text-3xl">Lista Zleceń</p>
+        <div class="grid grid-cols-4 grid-rows-5 w-full h-full ">
+            <div class="grid col-span-4 place-content-center ">
+                <p class="text-5xl text-shadow-lg shadow-green-800">Lista Zleceń</p>
             </div>
+            <div class="grid col-span-4 row-span-4 pr-5 pb-5">
+                <div class="grid grid-cols-4 pr-4 gap-2 grid-flow-col">
+                    <div class="navTitle rounded-tl-md">Nr zlecenia</div>
+                    <div class="navTitle">Dane pojazdu</div>
+                    <div class="navTitle">Opis naprawy</div>
+                    <div class="navTitle rounded-tr-md">Dane kontaktowe wlasciciela</div>
+                </div>
+                <div id="listaZlecen" class="grid grid-cols-4 row-span-5 gap-2  overflow-y-auto">
+
+                </div>
+            </div>
+                
         </div>
+            
     </div>
+    
+
+<script>
+    
+    var content = document.getElementById("listaZlecen");
+    var rows = 15;
+    var cols = 4;
+    for(var i =0;i<40;i++){
+        var div = document.createElement("div");
+        content.appendChild(div);
+        div.classList.add("bg-red-200","p-5","my-1");
+        div.textContent = "TEST"+i;
+            
+    }
+</script>
 </body>
+
 </html>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["*.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "Montserrat": ['mont','sans-serif']
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
 
