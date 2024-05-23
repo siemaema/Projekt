18d935e9cccd29fb51d937336de7ee182209c6d0
siemak <fkratiuk420@gmail.com>
2024-05-23 09:52:26 +0200

    kolejna proba

--- getCalendar.js
+++ getCalendar.js
@@ -1,136 +1,123 @@
-const previousMonthBtn = document.querySelector('.previous-month-btn');
-const nextMonthBtn = document.querySelector('.next-month-btn');
-const monthElement = document.querySelector('.current-month');
-const currentDateSpan = document.querySelector('.current-date');
-const calendarDaysContainer = document.querySelector('.calendar');
-let date = new Date();
-const getCurrentMonthName = (currentMonth) => {
-    const monthNameTab = [
-        'Styczeń',
-        'Luty',
-        'Marzec',
-        'Kwiecień',
-        'Maj',
-        'Czerwiec',
-        'Lipiec',
-        'Sierpień',
-        'Wrzesień',
-        'Październik',
-        'Listopad',
-        'Grudzień',
-    ];
-    return monthNameTab[currentMonth];
-};
+fetch('getRepairDates.php')
+    .then(response => response.json())
+    .then(repairDatesFromPHP => {
+        console.log(repairDatesFromPHP);
 
-const getCurrentDay = (currentDay) => {
-    const dayNameTab = [
-        'Poniedziałek',
-        'Wtorek',
-        'Środa',
-        'Czwartek',
-        'Piątek',
-        'Sobota',
-        'Niedziela',
-    ];
+        const previousMonthBtn = document.querySelector('.previous-month-btn');
+        const nextMonthBtn = document.querySelector('.next-month-btn');
+        const monthElement = document.querySelector('.current-month');
+        const currentDateSpan = document.querySelector('.current-date');
+        const calendarDaysContainer = document.querySelector('.calendar');
+        let date = new Date();
 
-    return dayNameTab[currentDay];
-};
+        const getCurrentMonthName = (currentMonth) => {
+            const monthNameTab = [
+                'Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 
+                'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'
+            ];
+            return monthNameTab[currentMonth];
+        };
 
-const countRepairsPerDay = (dateToCheck) => {
-    const dateToCheckTimestamp = dateToCheck.getTime();
-	console.log(repairDatesFromPHP);
-    let count = 0;
-    for (let i = 0; i < repairDatesFromPHP.length; i++) {
-        const repairDateTimestamp = repairDatesFromPHP[i];
-        if (dateToCheckTimestamp === repairDateTimestamp) {
-            count++;
-        }
-    }
-    return count;
-};
+        const getCurrentDay = (currentDay) => {
+            const dayNameTab = [
+                'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 
+                'Piątek', 'Sobota', 'Niedziela'
+            ];
+            return dayNameTab[currentDay];
+        };
 
-const initCalendar = () => {
-    const currentDay = date.getDay();
-    const currentYear = date.getFullYear();
-    let currentMonth = date.getMonth();
-    // Wyrzucenie nazwy miesiąca
-    monthElement.textContent = getCurrentMonthName(currentMonth);
-    //Wyrzucenie daty w formacie dd:mm:rrr
-    const currentDate = date.toLocaleDateString('pl-PL', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-    });
-    currentDateSpan.textContent = `${getCurrentDay(currentDay)} ${currentDate}`;
-    //Generowanie dni tygodnia kalendarza
-    for (let i = 0; i < 7; i++) {
-        const daySpan = document.createElement('span');
-        daySpan.textContent = getCurrentDay(i);
-        daySpan.classList.add('childTitle');
-        calendarDaysContainer.appendChild(daySpan);
-    }
-    //Generowanie zawartości kalendarza
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay(),
-        lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate(),
-        lastDayOfMonth = new Date(
-            currentYear,
-            currentMonth,
-            lastDateOfMonth
-        ).getDay(),
-        lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate();
+        const countRepairsPerDay = (dateToCheck) => {
+            const dateToCheckTimestamp = dateToCheck.setHours(0, 0, 0, 0); // Ustawienie godziny na 00:00:00
+            let count = 0;
+            for (let i = 0; i < repairDatesFromPHP.length; i++) {
+                const repairDateTimestamp = new Date(repairDatesFromPHP[i]).setHours(0, 0, 0, 0);
+                if (dateToCheckTimestamp === repairDateTimestamp) {
+                    count++;
+                }
+            }
+            return count;
+        };
 
-    for (let i = firstDayOfMonth; i > 1; i--) {
-        const dayBtn = document.createElement('button');
-        dayBtn.textContent = lastDateofLastMonth - i + 2;
-        dayBtn.classList.add('childContent', 'bg-gray-300');
-        calendarDaysContainer.appendChild(dayBtn);
-    }
+        const initCalendar = () => {
+            const currentDay = date.getDay();
+            const currentYear = date.getFullYear();
+            let currentMonth = date.getMonth();
 
-    for (let i = 1; i <= lastDateOfMonth; i++) {
-        const dayBtn = document.createElement('button');
-        
-        dayBtn.classList.add('childContent');
-        const repairsCount = countRepairsPerDay(new Date(currentYear, currentMonth, i));
-		dayBtn.textContent = repairsCount;
-        switch (repairsCount) {
-            case 1:
-                dayBtn.classList.add('bg-green-500');
-                break;
-			case 2:
-				dayBtn.classList.add('bg-green-500');
-				break;
-            case 3:
-                dayBtn.classList.add('bg-yellow-500');
-                break;
-            case 4:
-                dayBtn.classList.add('bg-red-500');
-                break;
-        }
-        calendarDaysContainer.appendChild(dayBtn);
-    }
-    if (lastDayOfMonth !== 0) {
-        for (let i = lastDayOfMonth; i < 6; i++) {
-            const dayBtn = document.createElement('button');
-            dayBtn.textContent = i - lastDayOfMonth + 1;
-            dayBtn.classList.add('childContent', 'bg-gray-300');
-            calendarDaysContainer.appendChild(dayBtn);
-        }
-    }
-};
+            monthElement.textContent = getCurrentMonthName(currentMonth);
 
-const refreshCalendar = () => {
-    calendarDaysContainer.innerHTML = '';
-    initCalendar();
-};
+            const currentDate = date.toLocaleDateString('pl-PL', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+            });
+            currentDateSpan.textContent = `${getCurrentDay(currentDay)} ${currentDate}`;
 
-nextMonthBtn.addEventListener('click', () => {
-    date.setMonth(date.getMonth() + 1);
-    refreshCalendar();
-});
+            for (let i = 0; i < 7; i++) {
+                const daySpan = document.createElement('span');
+                daySpan.textContent = getCurrentDay(i);
+                daySpan.classList.add('childTitle');
+                calendarDaysContainer.appendChild(daySpan);
+            }
 
-previousMonthBtn.addEventListener('click', () => {
-    date.setMonth(date.getMonth() - 1);
-    refreshCalendar();
-});
+            const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+            const lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+            const lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth).getDay();
+            const lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate();
 
-initCalendar();
+            for (let i = firstDayOfMonth; i > 1; i--) {
+                const dayBtn = document.createElement('button');
+                dayBtn.textContent = lastDateofLastMonth - i + 2;
+                dayBtn.classList.add('childContent', 'bg-gray-300');
+                calendarDaysContainer.appendChild(dayBtn);
+            }
+
+            for (let i = 1; i <= lastDateOfMonth; i++) {
+                const dayBtn = document.createElement('button');
+                dayBtn.classList.add('childContent');
+                const repairsCount = countRepairsPerDay(new Date(currentYear, currentMonth, i));
+                dayBtn.textContent = repairsCount;
+                switch (repairsCount) {
+                    case 1:
+                        dayBtn.classList.add('bg-green-500');
+                        break;
+                    case 2:
+                        dayBtn.classList.add('bg-green-500');
+                        break;
+                    case 3:
+                        dayBtn.classList.add('bg-yellow-500');
+                        break;
+                    case 4:
+                        dayBtn.classList.add('bg-red-500');
+                        break;
+                }
+                calendarDaysContainer.appendChild(dayBtn);
+            }
+
+            if (lastDayOfMonth !== 0) {
+                for (let i = lastDayOfMonth; i < 6; i++) {
+                    const dayBtn = document.createElement('button');
+                    dayBtn.textContent = i - lastDayOfMonth + 1;
+                    dayBtn.classList.add('childContent', 'bg-gray-300');
+                    calendarDaysContainer.appendChild(dayBtn);
+                }
+            }
+        };
+
+        const refreshCalendar = () => {
+            calendarDaysContainer.innerHTML = '';
+            initCalendar();
+        };
+
+        nextMonthBtn.addEventListener('click', () => {
+            date.setMonth(date.getMonth() + 1);
+            refreshCalendar();
+        });
+
+        previousMonthBtn.addEventListener('click', () => {
+            date.setMonth(date.getMonth() - 1);
+            refreshCalendar();
+        });
+
+        initCalendar();
+    })
+    .catch(error => console.error('Error fetching repair dates:', error));
